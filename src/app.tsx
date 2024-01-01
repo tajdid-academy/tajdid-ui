7cf5f34d76d94bdbbe31578c20984320dff43748
Mohib <mbmohib@gmail.com>
2024-01-01 06:52:31 +0600

    add-dialog: add dialog story variation

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,4 @@
-import { Button, Dialog } from './components';
 import './styles/app.css';
-import { EmailIcon } from '@/icons';
 
 export default function App() {
   return (
@@ -9,49 +7,7 @@ export default function App() {
         Use this to run a local development environment of the library for
         testing.
       </h1>
-      <div id="playground">
-        {/* Play inside this div */}
-
-        <Dialog.Root>
-          <Dialog.Trigger>
-            <Button>Open</Button>
-          </Dialog.Trigger>
-          <Dialog.Main>
-            <Dialog.Icon color="error">
-              <EmailIcon />
-            </Dialog.Icon>
-            <Dialog.Title>Logout</Dialog.Title>
-            <Dialog.Subtitle>Are you sure you want to logout?</Dialog.Subtitle>
-            <Dialog.Divider className="mt-6" />
-            <Dialog.Content>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-                consectetur facere voluptatum sit at, iure eum corrupti, quia
-                libero, consequatur cum magnam! Delectus sunt eius dignissimos
-                at eaque rem numquam.
-              </p>
-            </Dialog.Content>
-            <Dialog.CloseButton />
-            <Dialog.Action>
-              <Dialog.Close asChild>
-                <Button fullWidth variant="outlined" color="gray">
-                  No
-                </Button>
-              </Dialog.Close>
-              <Button
-                variant="contained"
-                fullWidth
-                color="error"
-                onClick={() => {
-                  console.log('Yes');
-                }}
-              >
-                Yes
-              </Button>
-            </Dialog.Action>
-          </Dialog.Main>
-        </Dialog.Root>
-      </div>
+      <div id="playground">{/* Play inside this div */}</div>
     </div>
   );
 }
--- src/components/dialog/dialog.stories.tsx
+++ src/components/dialog/dialog.stories.tsx
@@ -30,6 +30,55 @@ export default meta;
 export const Default = {
   args: {
     size: 'sm',
+    children: 'Dialog',
+  },
+  render: function Render() {
+    const [args] = useArgs();
+
+    return (
+      <Dialog.Root size={args.size}>
+        <Dialog.Trigger>
+          <Button>Open Dialog</Button>
+        </Dialog.Trigger>
+        <Dialog.Main>
+          <Dialog.Title>Logout</Dialog.Title>
+          <Dialog.Subtitle>Are you sure you want to logout?</Dialog.Subtitle>
+          <Dialog.Divider className="mt-6" />
+          <Dialog.Content>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              consectetur facere voluptatum sit at, iure eum corrupti, quia
+              libero, consequatur cum magnam! Delectus sunt eius dignissimos at
+              eaque rem numquam.
+            </p>
+          </Dialog.Content>
+          <Dialog.CloseButton />
+          <Dialog.Action>
+            <Dialog.Close asChild>
+              <Button fullWidth variant="outlined" color="gray">
+                No
+              </Button>
+            </Dialog.Close>
+            <Button
+              variant="contained"
+              fullWidth
+              color="error"
+              onClick={() => {
+                console.log('Yes');
+              }}
+            >
+              Yes
+            </Button>
+          </Dialog.Action>
+        </Dialog.Main>
+      </Dialog.Root>
+    );
+  },
+};
+
+export const WithHeaderIcon = {
+  args: {
+    size: 'sm',
     color: 'error',
     children: 'Dialog',
   },
@@ -39,7 +88,7 @@ export const Default = {
     return (
       <Dialog.Root size={args.size}>
         <Dialog.Trigger>
-          <Button>Open</Button>
+          <Button>Open Dialog</Button>
         </Dialog.Trigger>
         <Dialog.Main>
           <Dialog.Icon color={args.color}>
